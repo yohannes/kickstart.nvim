a283a583a6d94b829b3bf31b9943227c10335b00
Yohannes Wijaya <yohannes@hey.com>
2023-12-22 06:15:18 +0000

    Move plugins outside init.lua

---
+++ lua/custom/plugins/glow.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    -- Preview markdown code directly in your neovim terminal. Powered by Charm's glow
+    "ellisonleao/glow.nvim",
+    config = true,
+    cmd = "Glow"
+  },
+}
---
+++ lua/custom/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    -- Favorite theme
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+}
